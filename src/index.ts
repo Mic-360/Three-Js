92dbe8737627a55787c19a974760078f7384edbf
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-09-01 11:04:34 +0530

    rotate & scale

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,26 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 const mesh = new THREE.Mesh(geometry, material);
 
+mesh.scale.set(1, 2, 2);
+mesh.rotation.set(1, 0.5, 1, 'ZYX');
+
 scene.add(mesh);
 
+const AxisHelper = new THREE.AxesHelper(2);
+scene.add(AxisHelper);
+
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: 1265,
+  height: 620,
 };
 
 const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height);
-camera.position.z = 5;
-camera.position.y = 1;
-camera.position.x = 2;
+camera.position.set(1.2, 0.5, 5);
 scene.add(camera);
 
 const canvas = document.getElementById("webgl");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas as HTMLCanvasElement,
+    canvas: canvas as HTMLCanvasElement,
 });
 
 renderer.setSize(sizes.width, sizes.height);
