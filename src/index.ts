1df6cb3e081058e91e606ea161b4aa6009729c14
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-09-15 02:03:24 +0530

    camera fix still needed

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,12 @@ import {
   WebGLRenderer,
   AnimationMixer,
   AxesHelper,
-  TextureLoader,
   CubeTextureLoader,
   BoxGeometry,
   MeshStandardMaterial,
   Mesh,
-  SphereGeometry,
   Vector3,
+  Object3D,
 } from "three";
 import { DirectionalLight, AmbientLight, Clock } from "three";
 import { OrbitControls } from "@three-ts/orbit-controls";
@@ -46,45 +45,36 @@ const objectsToUpdate: any = [];
 //
 //
 
-let Xaxis = (Math.random() - 0.5) * 3;
-let Yaxis = 3;
-let Zaxis = (Math.random() - 0.5) * 3;
-debugObject.createSphere = () => {
-  createSphere(Math.random() * 0.5, {
-    x: Xaxis,
-    y: Yaxis,
-    z: Zaxis,
-  });
-};
+// let Xaxis = (Math.random() - 0.5) * 3;
+// let Yaxis = 3;
+// let Zaxis = (Math.random() - 0.5) * 3;
 
-gui.add(debugObject, "createSphere");
-
-debugObject.createBox = () => {
-  createBox(Math.random(), Math.random(), Math.random(), {
-    x: Xaxis,
-    y: Yaxis,
-    z: Zaxis,
-  });
-};
-gui.add(debugObject, "createBox");
+// debugObject.createBox = () => {
+//   createChar(Math.random(), Math.random(), Math.random(), {
+//     x: Xaxis,
+//     y: Yaxis,
+//     z: Zaxis,
+//   });
+// };
+gui.add(debugObject, "createChar");
 
 // Reset
-debugObject.reset = () => {
-  for (const object of objectsToUpdate) {
-    // Remove body
-    // object.body.removeEventListener('collide', playHitSound)
-    world.removeBody(object.body);
+// debugObject.reset = () => {
+//   for (const object of objectsToUpdate) {
+//     // Remove body
+//     // object.body.removeEventListener('collide', playHitSound)
+//     world.removeBody(object.body);
 
-    // Remove mesh
-    scene.remove(object.mesh);
-  }
-};
-gui.add(debugObject, "reset");
+//     // Remove mesh
+//     scene.remove(object.mesh);
+//   }
+// };
+// gui.add(debugObject, "reset");
 
 /**
  * Textures
  */
-const textureLoader = new TextureLoader();
+// const textureLoader = new TextureLoader();
 const cubeTextureLoader = new CubeTextureLoader();
 
 const environmentMapTexture = cubeTextureLoader.load([
@@ -190,123 +180,23 @@ floor.rotation.x = -Math.PI * 0.5;
 scene.add(floor);
 
 /**
- * Floor
+ * Floor END ----------------------
  */
 
 /*
  * Shapes and Materials INIT --------------------
  */
 
-//create sphere
-const sphereGeometry = new SphereGeometry(1, 20, 20);
-const sphereMaterial = new MeshStandardMaterial({
-  metalness: 0.3,
-  roughness: 0.4,
-  envMap: environmentMapTexture,
-  envMapIntensity: 0.5,
-});
-const createSphere = (radius: number, position: Vector3 | CANNON.Vec3) => {
-  // Three.js mesh
-  const mesh = new Mesh(sphereGeometry, sphereMaterial);
-  mesh.castShadow = true;
-  mesh.scale.set(radius, radius, radius);
-  mesh.position.copy(position as Vector3);
-  scene.add(mesh);
+let characterControls: Controller;
 
-  // Cannon.js body
-  const shape = new CANNON.Sphere(radius);
+let Avatar: string = "Soldier.glb"; //?Dynamic character selection
 
-  const body = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
-    shape: shape,
-    material: new CANNON.Material(),
-  });
-  body.position.copy(position as Vec3);
-  world.addBody(body);
-
-  // Save in objects
-  objectsToUpdate.push({ mesh, body });
-};
-
-// Create box
-const boxGeometry = new BoxGeometry(1, 1, 1);
-const boxMaterial = new MeshStandardMaterial({
-  metalness: 0.3,
-  roughness: 0.4,
-  envMap: environmentMapTexture,
-  envMapIntensity: 0.5,
-});
-
-const createBox = (
-  width: number,
-  height: number,
-  depth: number,
-  position: Vector3 | CANNON.Vec3
-) => {
-  // Three.js mesh
-  const mesh = new Mesh(boxGeometry, boxMaterial);
-  mesh.scale.set(width, height, depth);
-  mesh.castShadow = true;
-  mesh.position.copy(position as Vector3);
-  scene.add(mesh);
-
-  // Cannon.js body
-  const shape = new CANNON.Box(
-    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
-  );
-
-  const body = new CANNON.Body({
-    mass: 1,
-    position: position as CANNON.Vec3,
-    shape: shape,
-    material: new CANNON.Material(),
-  });
-  body.position.copy(position as Vec3);
-  world.addBody(body);
-
-  // Save in objects
-  objectsToUpdate.push({ mesh, body });
-};
-
-createBox(1, 1.5, 2, new CANNON.Vec3(0, 3, 0));
-
-/*
- * Shapes End
- */
-
-const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-// camera.position.set(15, 12, 5);
-
-const renderer = new WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true;
-
-const orbit = new OrbitControls(camera, renderer.domElement);
-orbit.minDistance = 2;
-orbit.enableDamping = true;
-orbit.maxDistance = 30;
-orbit.enablePan = false;
-orbit.maxPolarAngle = Math.PI / 2 - 0.05;
-orbit.update();
-
-light();
-
-var characterControls: Controller;
-
-new GLTFLoader().load("Soldier.glb", function (gltf) {
+let character = new GLTFLoader().load(Avatar, function (gltf) {
   const model = gltf.scene;
   model.traverse(function (object: any) {
     if (object.isMesh) object.castShadow = true;
   });
-  gltf.scene.scale.set(5, 5, 5), scene.add(model);
+  gltf.scene.scale.set(3, 3, 3), scene.add(model);
 
   const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
   const mixer = new AnimationMixer(model);
@@ -326,6 +216,53 @@ new GLTFLoader().load("Soldier.glb", function (gltf) {
     "Idle"
   );
 });
+
+// Create box
+
+const createChar = (
+  position: Vector3 | CANNON.Vec3
+) => {
+  // Three.js mesh
+  scene.add(character as unknown as Object3D<Event>);
+
+  // Cannon.js body
+  const body = character as CANNON.Body;
+  // body.position.copy(position as Vec3);
+  // world.addBody(body);
+
+  // Save in objects
+  objectsToUpdate.push({ character, body });
+};
+
+createChar(new Vector3(0, 0, 0));
+
+/*
+ * Shapes End
+ */
+
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(15, 12, 5);
+
+const renderer = new WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.minDistance = 2;
+orbit.enableDamping = true;
+orbit.maxDistance = 30;
+orbit.enablePan = false;
+orbit.maxPolarAngle = Math.PI / 2 - 0.05;
+orbit.update();
+
+light();
 
 const keysPressed = {};
 document.addEventListener(
@@ -352,7 +289,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const clock = new Clock();
-let oldElapsedTime = 0;
+// let oldElapsedTime = 0;
 
 function animate() {
   let mixerUpdateDelta = clock.getDelta();
@@ -361,14 +298,9 @@ function animate() {
   }
   orbit.update();
 
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - oldElapsedTime;
-  oldElapsedTime = elapsedTime;
-
-  for (const object of objectsToUpdate) {
-    object.mesh.position.copy(object.body.position);
-    object.mesh.quaternion.copy(object.body.quaternion);
-  }
+  // const elapsedTime = clock.getElapsedTime();
+  // const deltaTime = elapsedTime - oldElapsedTime;
+  // oldElapsedTime = elapsedTime;
 
   // Update controls
   controls.update();
@@ -378,7 +310,7 @@ function animate() {
   renderer.render(scene, camera);
 
   // Update physics
-  world.step(1 / 60, deltaTime, 3);
+  // world.step(1 / 60, deltaTime, 3);
 
   requestAnimationFrame(animate);
 }
